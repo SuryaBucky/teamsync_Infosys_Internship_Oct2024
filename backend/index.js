f5c7783c9258725afa59050828f92292c5aa9ed5
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-17 11:02:09 +0530

    Added cors and changed port to 3001 for backend to avoid conflict with client port

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,16 @@ const app = express();
 const adminRouter = require("./routes/admin")
 const userRouter = require("./routes/user");
 const projectRouter = require("./routes/project");
+const cors=require("cors");
 
 // Middleware for parsing request bodies
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 app.use("/project", projectRouter)
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
