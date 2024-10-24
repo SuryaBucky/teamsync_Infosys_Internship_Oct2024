7622ce86de319b828fa34864cc126ab58c2f5a36
Prabhala Sowmya <sowmyaprabhala2004@gmail.com>
2024-10-24 22:09:00 +0530

    added  backend for update deadline

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,12 +1,13 @@
 const express = require("express");
 // Import the validation and creation middleware
 require("dotenv").config();
-const { validateTaskCreation, createTask,viewTasksByProject,validateAddAssignee,addAssignee } = require('../middlewares/TaskMiddlewares'); 
+const { validateTaskCreation, createTask,viewTasksByProject,validateAddAssignee,addAssignee,validateUpdateDeadline,updatedeadline } = require('../middlewares/TaskMiddlewares'); 
 
 const router = express.Router();
 
 router.post('/project/:project_id/create-task', validateTaskCreation, createTask);
 router.get('/project/:project_id/view-tasks',viewTasksByProject);
 router.post('/:task_id/add-assignee', validateAddAssignee, addAssignee);
+router.put('/:task_id/update-deadline', validateUpdateDeadline,updatedeadline);
 
 module.exports = router;
