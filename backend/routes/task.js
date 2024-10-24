9612f500995b68a06142e7f8e248783c6056ba04
Prabhala Sowmya <sowmyaprabhala2004@gmail.com>
2024-10-24 22:27:53 +0530

    added edit details of a task
    details that can be edited are the task description and priority

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,7 +1,7 @@
 const express = require("express");
 // Import the validation and creation middleware
 require("dotenv").config();
-const { validateTaskCreation, createTask,viewTasksByProject,validateAddAssignee,addAssignee,validateUpdateDeadline,updatedeadline } = require('../middlewares/TaskMiddlewares'); 
+const { validateTaskCreation, createTask,viewTasksByProject,validateAddAssignee,addAssignee,validateUpdateDeadline,updatedeadline,validateEditDetails,editTaskDetails } = require('../middlewares/TaskMiddlewares'); 
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.post('/project/:project_id/create-task', validateTaskCreation, createTask
 router.get('/project/:project_id/view-tasks',viewTasksByProject);
 router.post('/:task_id/add-assignee', validateAddAssignee, addAssignee);
 router.put('/:task_id/update-deadline', validateUpdateDeadline,updatedeadline);
+router.put('/:task_id/edit-details', validateEditDetails,editTaskDetails);
 
 module.exports = router;
