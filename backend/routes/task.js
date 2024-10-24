9db9201c18a9ea58078c0fdc9866aa1a46587ca3
Prabhala Sowmya <sowmyaprabhala2004@gmail.com>
2024-10-24 11:42:47 +0530

    added backend routes and middleware for adding assignees

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
-const { validateTaskCreation, createTask,viewTasksByProject } = require('../middlewares/TaskMiddlewares'); // Import the validation and creation middleware
+// Import the validation and creation middleware
 require("dotenv").config();
+const { validateTaskCreation, createTask,viewTasksByProject,validateAddAssignee,addAssignee } = require('../middlewares/TaskMiddlewares'); 
 
 const router = express.Router();
 
 router.post('/project/:project_id/create-task', validateTaskCreation, createTask);
 router.get('/project/:project_id/view-tasks',viewTasksByProject);
+router.post('/:task_id/add-assignee', validateAddAssignee, addAssignee);
 
 module.exports = router;
