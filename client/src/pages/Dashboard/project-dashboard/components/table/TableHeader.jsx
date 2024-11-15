26af654f3aa417064b4703af62f1052e2b057594
Tejashree1962 <tejugparab555@gmail.com>
2024-11-15 18:15:06 +0530

    Fixes #244 ProjectTable content overflow on resize

--- client/src/pages/Dashboard/project-dashboard/components/table/TableHeader.jsx
+++ client/src/pages/Dashboard/project-dashboard/components/table/TableHeader.jsx
@@ -1,12 +1,12 @@
 export const TableHeader = () => (
   <tr className="border-b bg-gray-100">
-    <th className="text-left py-4 px-6 font-medium text-xs">Name</th>
-    <th className="text-left py-4 px-6 font-medium text-xs">Status</th>
-    <th className="text-left py-4 px-6 font-medium text-xs">About</th>
-    <th className="text-left py-4 pe-6 ps-3 font-medium text-xs">Members</th>
-    <th className="text-left py-4 pe-6 ps-3 font-medium text-xs">Progress</th>
-    <th className="text-left py-4 pe-6 ps-5 font-medium text-xs">Deadline</th>
-    <th className="text-left py-4 pe-6 ps-5  font-medium text-xs">Priority</th> {/* New Priority column */}
+    <th className="text-left py-3 px-2 md:px-4 font-medium text-xs">Name</th>
+    <th className="text-left py-3 px-2 md:px-4 font-medium text-xs">Status</th>
+    <th className="py-3 px-2 md:px-4 font-medium text-xs">About</th>
+    <th className="text-left py-3 px-2 md:px-4 font-medium text-xs">Members</th>
+    <th className="text-left py-3 px-2 md:px-4 font-medium text-xs">Progress</th>
+    <th className="text-left py-3 px-2 md:px-4 font-medium text-xs">Deadline</th>
+    <th className="text-left py-3 px-2 md:px-4 font-medium text-xs">Priority</th>
     <th className="w-10"></th>
   </tr>
 );
